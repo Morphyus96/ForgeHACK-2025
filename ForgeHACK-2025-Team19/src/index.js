7dc7a3957418963b4a9dd6975ba45eb8904ed2f3
Ronald <z5420648@student.unsw.edu.au>
2025-02-11 16:34:09 +1100

    updated to lily's working version

--- ForgeHACK-2025-Team19/src/index.js
+++ ForgeHACK-2025-Team19/src/index.js
@@ -13,12 +13,16 @@ export const getIssues = async (payload, requestContext) => {
   const response = await api.asApp().requestJira(route`/rest/api/3/search?jql=${jql}`);
   const data = await response.json();
   const cleanData = await extractIssueDetails(data);
+
+  // TODO: Categorise each issue based on due date and priority 
+  // 
+
   return cleanData;
 }
 
 // Extract issue details from the response
 export const extractIssueDetails = async (data) => {
-  // console.log(Extracting issue details from response: ${JSON.stringify(data)});
+  // console.log(`Extracting issue details from response: ${JSON.stringify(data)}`);
   return data.issues.map(issue => ({
       key: issue.key,
       summary: issue.fields.summary,
@@ -27,5 +31,7 @@ export const extractIssueDetails = async (data) => {
       assignee: issue.fields.assignee ? issue.fields.assignee.displayName : 'Unassigned',
       status: issue.fields.status.name,
       labels: issue.fields.labels > 0 ? issue.fields.labels : 'No labels set',
-  }));
+      daysDue: issue.fields.duedate ? Math.ceil((new Date(issue.fields.duedate) - new Date()) / (1000 * 3600 * 24)) : 'No due date',
+      dueDate: issue.fields.duedate ? issue.fields.duedate : 'No due date',
+    }));
 }
